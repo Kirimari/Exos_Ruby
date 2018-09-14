dd39cc02d061a153802e68817e9ba4a79dc4ba7d
Kirimari <ait.mus06@gmail.com>
2018-09-14 16:26:27 +0200

    2nd exo avec question sur print/puts

--- exo_02.rb
+++ exo_02.rb
@@ -1,2 +1,3 @@
-print "Bonjour monde!"
+puts "Bonjour monde!"
+print "Et avec une voix sexy, ça donne : Bonjour monde!"
 #Le résultat ne saute pas de ligne à la fin pour donner un autre résultat. Si on avait écrit : print "Bonjour monde!","Comment vas-tu ?" le résultat serait : Bonjour monde!Comment vas-tu?

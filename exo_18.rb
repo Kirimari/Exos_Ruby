23ef7cc3a9ba973af5a88893c6fb07a2ef979418
Kirimari <ait.mus06@gmail.com>
2018-09-26 11:49:59 +0200

    Exos Ruby THP

--- exo_18.rb
+++ exo_18.rb
@@ -1,12 +1,16 @@
+a = []
+
 for n in 1..50
+
+	if n <= 9
+
+    	a << "jean.dupont.0#{n}@email.com"
+
+	else
+
+		a << "jean.dupont.#{n}@email.com"
     
-    if n <= 9
-    
-    puts %["jean.dupont.0#{n}@email.com"]
-        
-    else 
-        
-    puts %["jean.dupont.#{n}@email.com"]
-        
-    end 
+	end
 end
+ 
+ print a 
--- exo_19.rb
+++ exo_19.rb
@@ -1,12 +1,18 @@
+a = []
+
 for n in 1..50
-    
-    next if n % 2 != 0 
-    
-    puts %["jean.dupont.0#{n}@email.com"]
-        
-    else
-        
-    puts %["jean.dupont.#{n}@email.com"]
-    
-    end 
+
+	if n <= 9
+
+    	a << "jean.dupont.0#{n}@email.com"
+
+	else
+
+		a << "jean.dupont.#{n}@email.com"
+	end
 end
+
+print a
+
+
+ 

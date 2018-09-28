ab9e8f6b936903fb3023c1ce5ddd067f1e24de72
Kirimari <ait.mus06@gmail.com>
2018-09-28 17:38:53 +0200

    Limite de 25 ajoutée

--- exo_21.rb
+++ exo_21.rb
@@ -4,10 +4,15 @@ n = gets.chomp.to_i
 
 etage = 1 
 space = n - 1 
-    while etage <= n 
+    while etage <= n && n <= 25
         
         puts " " * space + "#" * etage
         
         etage = etage + 1
         space = space - 1  
     end
+
+
+
+
+
